9957b896af403965af82df4e22c15ac5539a6918
Neitto <christcarbo@hotmail.com>
2016-06-14 16:32:33 +0200

    Version 8 supp - 14.06.2016 (16:32)
    Le site.

--- site/ressources/lib/header.php
+++ site/ressources/lib/header.php
@@ -22,7 +22,7 @@
         <link href="../../ressources/css/semantic.css" rel="stylesheet">
         <link href="../../ressources/css/table.css" rel="stylesheet">
         <link href="../../ressources/css/icon.css" rel="stylesheet">
-        <link href="../../ressources/css/semantic.min.css" rel="stylesheet">
+        <link href="../../ressources/css/divider.css" rel="stylesheet">
 
         <script src="http://www.amcharts.com/lib/3/amcharts.js" type="text/javascript"></script>
         <script src="http://cdn.amcharts.com/lib/3/serial.js" type="text/javascript"></script>
